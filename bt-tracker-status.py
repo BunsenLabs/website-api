ea78f6eb26352a4049d0c127ffca8756197996f1
Jens John <dev@2ion.de>
2016-12-01 18:26:18 +0100

    api:bt-status:decouple backend data retrieval from frontend

--- bt-tracker-status.py
+++ bt-tracker-status.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python3
 
 from bottle import run, route, abort
+import threading
+import time
 import requests
 
-def fetchot(param):
-    records = []
-    try:
-        data = requests.get("""http://127.0.0.1:6969/stats?{}""".format(param)).text
-    except:
-        return None
-    for line in data.split('\n'):
-        records.append(line)
-    return records[:-1]
+PUBLIC = {}
+
+class Fetcher(threading.Thread):
+
+    def run(self):
+        self.evt = threading.Event()
+        self.update_data()
+        while not self.evt.wait(timeout=10):
+            self.update_data()
+
+    def update_data(self):
+        _public = {"torrents":{}}
+        data = self.fetchot("mode=tpbs&format=txt")
+        if data is None:
+          print("No data")
+          return True
+        for line in data:
+            [ hash, seeders, leechers ] = line.split(":", 3)
+            _public["torrents"][hash.lower()] = { "s":seeders, "l":leechers }
+        meta = self.fetchot("mode=completed")
+        _public["completed"] = self.fetchot("mode=completed")[0]
+        _public["ts"] = int(time.time())
+        print(_public)
+        global PUBLIC
+        PUBLIC = _public
+
+    def fetchot(self, param):
+        records = []
+        try:
+            data = requests.get("""http://127.0.0.1:6969/stats?{}""".format(param)).text
+        except:
+            return None
+        for line in data.split('\n'):
+            records.append(line)
+        return records[:-1]
 
 @route('/tracker/status')
 def torrent_status():
-    data = fetchot("mode=tpbs&format=txt")
-    if data is None:
-        abort(500, "You know")
-    public = {}
-    for line in data:
-        [ hash, seeders, leechers ] = line.split(":", 3)
-        public[hash.lower()] = { "s":seeders, "l":leechers }
-    return public
+    return PUBLIC
 
 if __name__ == "__main__":
+    fetcher = Fetcher()
+    fetcher.start()
     run(host="localhost", port=10101)
