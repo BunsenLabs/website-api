6e56e32af8d54fcc6dd9c3d48849e2c644c5d14b
Jens John <dev@2ion.de>
2018-07-10 20:09:07 +0200

    setup: Bump version to 1.0.3
      * Add <id> identifiers to the Atom feed and all of its entries. This
        UUID is based on the domain namespace forums.bunsenlabs.org (feed) or
        the link to the source forum post (entries).

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "blwwwapi",
-        version = "1.0.2",
+        version = "1.0.3",
         install_requires = [
                 "bottle",
                 "beautifulsoup4",
