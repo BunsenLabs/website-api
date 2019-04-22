921e3ac3d93f5f73d4bf4b5ff4d83a7d8b432e75
Jens John <dev@2ion.de>
2019-04-22 11:05:32 +0200

    message: Log schema loads

--- blwwwapi/message.py
+++ blwwwapi/message.py
@@ -1,16 +1,19 @@
 from blwwwapi import __SCHEMAPATH__
+from blwwwapi.logging import named_logger
 from cerberus import Validator
 from dataclasses import dataclass, field
 import os
 import yaml
 
 SCHEMA = {}
+LOGGER = named_logger(__name__)
 
 for obj in os.scandir(__SCHEMAPATH__):
   if obj.is_file():
     with open(obj.path, "r") as FILE:
       verb = obj.name.split(".")[0]
       SCHEMA[verb] = yaml.safe_load(FILE)
+      LOGGER.info(f"Loaded schema from {obj.path}: {verb}")
 
 @dataclass(frozen=True)
 class Message:
