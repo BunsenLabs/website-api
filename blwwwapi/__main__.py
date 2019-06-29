f930939ae9a59066e441db7fd604bab81933b967
Jens John <dev@2ion.de>
2019-06-29 13:28:14 +0200

    main: Load options early so we can handle --help without bootstrapping

--- blwwwapi/__main__.py
+++ blwwwapi/__main__.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python3
-from blwwwapi.logging import named_logger
-from blwwwapi.message import Message
-from blwwwapi.workers.news import News
-from blwwwapi.workers.tracker import Tracker
-from bottle import run, route, abort, response, install
-from queue import Queue, Empty
-from typing import Any
-import blwwwapi.options
-import os
-import pickle
-import sys
 
-logger = named_logger()
+import sys
 
 EMPTY={}
 ENDPOINT_DATA = {}
 
-def check_namespace_access(thread_id: str, endpoint: str) -> bool:
-  if not endpoint.startswith(f"/{thread_id}"):
-    logger.warning(f"Thread {thread_id} tried accessing foreign endpoint {endpoint}")
-    return False
-  return True
-
 def main() -> int:
+  # Load options early to handle --help efficiently.
+  import blwwwapi.options
+  opts = blwwwapi.options.get()
+
+  from blwwwapi.logging import named_logger
+  from blwwwapi.message import Message
+  from blwwwapi.workers.news import News
+  from blwwwapi.workers.tracker import Tracker
+  from bottle import run, route, abort, response, install
+  from queue import Queue, Empty
+  from typing import Any
+  import os
+  import pickle
+
+  logger = named_logger()
   queue = Queue()
+  whitelist = opts.workers.split(",")
+  classes = [ News, Tracker ]
+  params = (opts,queue,)
+  threads = []
+
+  def check_namespace_access(thread_id: str, endpoint: str) -> bool:
+    if not endpoint.startswith(f"/{thread_id}"):
+      logger.warning(f"Thread {thread_id} tried accessing foreign endpoint {endpoint}")
+      return False
+    return True
 
   def _put(msg):
     id = msg.sender
@@ -74,12 +82,7 @@ def main() -> int:
   def r_tracker_status():
     return poll_queue("/tracker/status")
 
-  opts = blwwwapi.options.get()
-  whitelist = opts.workers.split(",")
-  classes = [ News, Tracker ]
-  params = (opts,queue,)
-  threads = []
-
+ 
   for cls in classes:
     if cls._id in whitelist:
       logger.info(f"Starting worker: {cls._id}")
